ba7400f347128ef89f8dc4263b6b351679b4be7c
Alan Shepherd <alkalinemusic@gmail.com>
2019-07-02 07:56:11 -0400

    starting of tkinter gui

--- make_sentence_case_GUI.py
+++ make_sentence_case_GUI.py
@@ -5,6 +5,16 @@ import tkMessageBox
 import tkFileDialog
 
 
+root = Tk()
+root.config(bg='gray14')
+
+class SentenceCase(object):
+	"""docstring for SentenceCase"""
+	def __init__(self, master):
+		self.master = master
+		master.title = ("Make Sentence Case")
+		
+
 sentence = sys.argv[1]
 
 rtn = re.split('([.!?] *)', sentence)
@@ -16,3 +26,9 @@ for each in rtn:
 
 print("\n\n\n")
 print(''.join(str_pieces))
+
+
+
+my_gui = SentenceCase(root)
+
+root.mainloop()
