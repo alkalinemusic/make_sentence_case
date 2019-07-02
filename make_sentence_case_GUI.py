3ecd79227b66b0a63ccda1474279bdb19fbc5379
Alan Shepherd <alkalinemusic@gmail.com>
2019-07-02 07:52:13 -0400

    creating GUI for application

---
+++ make_sentence_case_GUI.py
@@ -0,0 +1,18 @@
+import sys
+import re
+from tkinter import * 
+import tkMessageBox
+import tkFileDialog
+
+
+sentence = sys.argv[1]
+
+rtn = re.split('([.!?] *)', sentence)
+
+str_pieces = []
+
+for each in rtn:
+	str_pieces.append(each.capitalize())
+
+print("\n\n\n")
+print(''.join(str_pieces))
