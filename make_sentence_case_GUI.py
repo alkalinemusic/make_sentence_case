a18b480e17ce96a5ecf9aaa7777c391fbe6bc85c
Alan Shepherd <alkalinemusic@gmail.com>
2019-07-06 17:53:40 -0400

    reading entered values in text entry field

--- make_sentence_case_GUI.py
+++ make_sentence_case_GUI.py
@@ -1,44 +1,55 @@
 import sys
 import re
-from tkinter import * 
+import tkinter as tk
 # import tkMessageBox
 # import tkFileDialog
 
 
-root = Tk()
+root = tk.Tk()
 root.config(bg='gray14')
 
 class SentenceCase:
 	"""docstring for SentenceCase"""
 	def __init__(self, master):
-		super(SentenceCase, self).__init__()
+		# super(SentenceCase, self).__init__()
 		self.master = master
 		master.title("Make Sentence Case")
 
-	sentence_text = StringVar()
-	self.text_entry = Entry(master, textvariable=sentence_text)
-	self.text_entry.config(fg='bisque1', bg='gray28', font='bold')
-	text_entry_label = Label(root, text="Enter sentence/paragraph here", font='bold')
-	text_entry_label.config(fg='bisque3', bg='gray14')
-	text_entry_label.grid(row=1, column=0, sticky=W)								
+		sentence_text = tk.StringVar()
+		self.text_entry = tk.Entry(master, textvariable=sentence_text)
+		self.text_entry.config(fg='bisque1', bg='gray28', font='bold')
+		text_entry_label = tk.Label(root, text="Enter sentence/paragraph here", font='bold')
+		text_entry_label.config(fg='bisque3', bg='gray14')
+		text_entry_label.grid(row=1, column=0, sticky=tk.W)								
 
 
-	self.projectNameEntry.grid(row=1, column=1, columnspan=40, sticky=W+E)	
 
-	def sentence_conversion():
-		pass
-		# sentence = sys.argv[1]
+		self.text_entry.grid(row=1, column=1, columnspan=40, sticky=tk.W+tk.E)	
 
-		# rtn = re.split('([.!?] *)', sentence)
+		def sentence_conversion(*args):
+			print(sentence_text.get())
+			# sentence = self.text_entry
+			# rtn = re.split('([.!?] *)', sentence)
+			# str_pieces = []
+			# for each in rtn:
+			# 	str_pieces.append(each.capitalize())
 
-		# str_pieces = []
 
-		# for each in rtn:
-		# 	str_pieces.append(each.capitalize())
+			# sentence = sys.argv[1]
 
-		# print("\n\n\n")
-		# print(''.join(str_pieces))
+			# rtn = re.split('([.!?] *)', sentence)
 
+			# str_pieces = []
+
+			# for each in rtn:
+			# 	str_pieces.append(each.capitalize())
+
+			# print("\n\n\n")
+			# print(''.join(str_pieces))
+
+		self.convert_button = tk.Button(master, text="Convert", fg='bisque1',bg='gray28',
+								 font='bold', command=sentence_conversion)
+		self.convert_button.grid(row=2, column=0, sticky=tk.W)
 	
 
 my_gui = SentenceCase(root)
